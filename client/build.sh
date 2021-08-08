c394329978ccd158f20c1f67b7d36bbe713347e6
Jack Hopkins <jack.hopkins@me.com>
2021-08-08 22:43:41 +0100

    asdasd

--- client/build.sh
+++ client/build.sh
@@ -1,4 +1,4 @@
-# /bin/bash
+#!/bin/bash
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 216370203482.dkr.ecr.eu-west-2.amazonaws.com
 docker build . -t paperclip-client
 docker tag paperclip-client 216370203482.dkr.ecr.eu-west-2.amazonaws.com/paperclip-client
