2d05e418a6d38e189d62b413884908118c200b77
Jack Hopkins <jack@raveler.co.uk>
2023-04-19 19:22:32 -0400

    Add CraftItem controller
    Description: This commit adds a new controller called CraftItem that allows the agent to craft an item from their inventory. The CraftItem controller inherits from the Action controller and takes in an entity name and quantity as parameters. If the agent has enough resources, the entity is crafted and placed in their inventory. If the crafting is not successful, an exception is raised.

---
+++ client/controllers/craft_item.py
@@ -0,0 +1,28 @@
+from controllers.action import Action
+from factorio_instance import PLAYER
+
+
+class CraftItem(Action):
+
+    def __init__(self, connection):
+        Action.__init__(self, connection)
+
+    def __call__(self, entity: str, quantity: int = 1) -> bool:
+        """
+        The agent places an entity e at local position (x, y) if the agent has
+        enough resources. If the agent chooses to place an empty entity at (x, y), any entity at
+        (x, y) is removed. If the agent chooses to place an entity where there is already one,
+        the previous entity is first removed and the new entity replaces it.
+        :param x: X position relative to the agent as the origin (0).
+        :param y: Y position relative to the agent as the origin (0).
+        :param entity: Entity to place from inventory
+        :param direction: Cardinal direction to place entity
+        :return: True if action carried out, False if no-op
+        """
+        success, elapsed = self._send('craft', PLAYER, entity.replace("_", "-"), quantity)
+        if success != 1:
+            if success is None:
+                raise Exception(f"Could not craft a {entity}", "Ingredients cannot be crafted by hand.")
+            else:
+                raise Exception(f"Could not craft a {entity}", success)
+        return "Crafting successful"
