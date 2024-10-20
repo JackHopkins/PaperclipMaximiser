0c3b7b3b83b13ba8c08a0a51a76265ba2ed47d03
Jack Hopkins <jack.hopkins@me.com>
2024-10-19 22:51:28 -0300

    feat(cloudformation): a cloudformation template to deploy factorio servers remotely
    aws

--- src/actions/get_entities.lua
+++ src/actions/get_entities.lua
@@ -1,6 +1,12 @@
-global.actions.get_entities = function(player_index, radius, entity_names_json, x, y)
+global.actions.get_entities = function(player_index, radius, entity_names_json, position_x, position_y)
     local player = game.get_player(player_index)
-    local position = (x == 0 and y == 0) and player.position or {x = x, y = y}
+    local position
+    if position_x and position_y then
+        position = {x = tonumber(position_x), y = tonumber(position_y)}
+    else
+        position = player.position
+    end
+    --local position = (x == 0 and y == 0) and player.position or {x = x, y = y}
     radius = tonumber(radius) or 5
 
     local entity_names = game.json_to_table(entity_names_json) or {}
