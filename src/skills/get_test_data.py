e0ad2f3ca2dc291c30b772ffb88639e379416175
Mart <mart@paperplane.ai>
2024-10-27 16:33:59 +0000

    added skill extraction

--- src/skills/get_test_data.py
+++ src/skills/get_test_data.py
@@ -20,7 +20,22 @@ def extract_skills_from_test(test_file):
         initial_inv = initial_inv.replace(" ", "").replace("\n", "")
         # make it a dict
         initial_inv = eval(initial_inv)
-    print(len(test_file))
+    skills = []
+    skill_defs = test_files[1:]
+    for skill_def in skill_defs:
+        function_parts = skill_def.split("\n")
+        function_name = function_parts[0]
+        function_name = function_name.split("(")[0]
+        function_parts = function_parts[1:]
+        for part_idx, function_part in enumerate(function_parts):
+            if "    " not in function_part:
+                continue
+            function_part.replace("game.", "")
+            function_part = function_part[4:]
+            function_parts[part_idx] = function_part
+        function_parts = "\n".join(function_parts)
+        skills.append(function_parts)
+    return skills
 
 
 def get_skills_from_func_tests(func_test_folder):
