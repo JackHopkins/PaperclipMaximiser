9bfef022012fe40eef656666bca393b7d30fb765
Mart <mart@paperplane.ai>
2024-11-11 10:27:40 +0000

    removed testing code

--- src/datasets/dataset_creator.py
+++ src/datasets/dataset_creator.py
@@ -1,7 +1,3 @@
-import sys
-sys.path.append(r"C:\Users\martb\Documents\paperpclip_max\PaperclipMaximiser\src")
-sys.path.append(r"C:\Users\martb\Documents\paperpclip_max\PaperclipMaximiser")
-
 import re
 import os
 import ast
