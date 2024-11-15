6dbef43b1f4897f1421b69235d305d057b6c76b5
Mart <mart@paperplane.ai>
2024-11-15 17:03:44 +0000

    fixed reward bug

--- src/actions/reward.lua
+++ src/actions/reward.lua
@@ -333,16 +333,49 @@ global.goal = nil
 global.actions.score = function()
     --player = game.players[1]
     local production_score = production_score.get_production_scores()
+    game.print(dump(production_score))
     production_score["player"] = production_score["player"] - global.initial_score["player"]
     local goal_description = game.players[1].get_goal_description()
+
     if global.goal ~= goal_description then
       global.goal = goal_description
-      --game.print(goal_description)
-      if goal_description ~= nil then
+      game.print(goal_description)
+      if goal_description ~= nil and #goal_description > 1 then
         production_score["goal"] = goal_description[1]:gsub("-", "_")
       end
       return dump(production_score)
     end
+    return dump(production_score)
+end
 
-    return scores
+global.actions.production_stats = function(player)
+    local production_diff = {}
+    local consumption_diff = {}
+    -- Get total production counts for force
+    local force = game.forces.player
+
+    local item_input_counts = force.item_production_statistics.input_counts
+    local item_production_counts = force.item_production_statistics.output_counts
+    local fluid_input_counts = force.fluid_production_statistics.input_counts
+    local fluid_production_counts = force.fluid_production_statistics.output_counts
+
+    for name, count in pairs(item_input_counts) do
+        consumption_diff[name] = count
+    end
+
+    for name, count in pairs(item_production_counts) do
+        production_diff[name] = count
+    end
+
+    for name, count in pairs(fluid_input_counts) do
+        consumption_diff[name] = count
+    end
+
+    for name, count in pairs(fluid_production_counts) do
+        production_diff[name] = count
+    end
+    return {
+        output = consumption_diff,
+        input = production_diff
+    }
 end
