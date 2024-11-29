f88184d1bc75c9506959b47ed3044888694b9be1
Mart <mart@paperplane.ai>
2024-11-29 18:03:34 +0000

    created a test

--- src/tests/test_production_stats.py
+++ src/tests/test_production_stats.py
@@ -4,32 +4,29 @@ from factorio_instance import FactorioInstance
 from factorio_rcon_utils import _lua2python
 from factorio_types import Resource
 
-inventory = {
-    'iron-plate': 50,
-    'coal': 50,
-    'copper-plate': 50,
-    'iron-chest': 2,
-    'burner-mining-drill': 3,
-    'electric-mining-drill': 1,
-    'assembling-machine-1': 1,
-    'stone-furnace': 9,
-    'transport-belt': 50,
-    'boiler': 1,
-    'burner-inserter': 32,
-    'pipe': 15,
-    'steam-engine': 1,
-    'small-electric-pole': 10
-}
-instance = FactorioInstance(address='localhost',
+class TestProductionStats(unittest.TestCase):
+    def test_production_stats(self):
+        inventory = {
+                'iron-plate': 50,
+                'coal': 50,
+                'copper-plate': 50,
+                'iron-chest': 2,
+                'burner-mining-drill': 3,
+                'electric-mining-drill': 1,
+                'assembling-machine-1': 1,
+                'stone-furnace': 9,
+                'transport-belt': 50,
+                'boiler': 1,
+                'burner-inserter': 32,
+                'pipe': 15,
+                'steam-engine': 1,
+                'small-electric-pole': 10
+            }
+        instance = FactorioInstance(address='localhost',
                             bounding_box=200,
                             tcp_port=27015,
                             fast=True,
                             inventory=inventory)
-
-
-class TestProductionStats(unittest.TestCase):
-    def test_production_stats(self):
-
         instance.move_to(instance.nearest(Resource.IronOre))
         instance.harvest_resource(instance.nearest(Resource.IronOre), quantity=10)
 
