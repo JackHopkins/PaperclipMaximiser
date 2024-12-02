afb9f70a1762856460fc0285c06cf8b66ae59dae
Mart <mart@paperplane.ai>
2024-12-02 11:25:23 +0000

    added water and steam achievement tests

--- src/tests/test_achievements.py
+++ src/tests/test_achievements.py
@@ -1,10 +1,11 @@
 import unittest
-
+import sys
+sys.path.append(r"C:\Users\martb\Documents\paperpclip_max\PaperclipMaximiser\src")
+sys.path.append(r"C:\Users\martb\Documents\paperpclip_max\PaperclipMaximiser")
 from factorio_instance import FactorioInstance
 from utils import eval_program_with_achievements
 
-class TestAchievements(unittest.TestCase):
-    def test_achievements(self):
+def test_achievements_from_scratch():
         instance = FactorioInstance(address='localhost',
                                 bounding_box=200,
                                 tcp_port=27015,
@@ -26,5 +27,102 @@ class TestAchievements(unittest.TestCase):
         ground_truth_achievement = {'static': {'stone-furnace': 1, 'coal': 10, 'stone': 10, 'copper-ore': 10}, 'dynamic': {'copper-plate': 5}}
         assert achievements == ground_truth_achievement
 
+def test_achievements_fluid_and_steam():
+        """
+        This test might fail in some map cases due to place_entity_next_to direction being incompatible with the nearest water source
+        This has been tested in fact lab, game speed atleast 5
+        """
+        instance = FactorioInstance(address='localhost',
+                                bounding_box=200,
+                                tcp_port=27015,
+                                fast=True,
+                                #cache_scripts=False,
+                                inventory={"offshore-pump":1,
+                                           "boiler":1,
+                                           "coal":4,
+                                           "pipe":30,
+                                           "steam-engine":1})
+
+        test_string = "pos = nearest(Resource.Water)\nmove_to(pos)\npump = place_entity(Prototype.OffshorePump, position = pos)\nboiler = place_entity_next_to(Prototype.Boiler, reference_position = pump.position, spacing =2, direction = Direction.UP)\nconnect_entities(pump, boiler, Prototype.Pipe)\nsleep(5)"
+        # dynami cwater
+        _, _, _, achievements = eval_program_with_achievements(instance, test_string)
+        assert len(achievements["static"].keys())==0
+        assert len(achievements["dynamic"].keys())==1
+        assert "water" in achievements["dynamic"].keys()
+
+        # add another entity that shouldnt add any achievements
+        test_string = "entities = get_entities()\nboiler = [entity for entity in entities if entity.name == 'boiler'][0]\nsteam_engine = place_entity_next_to(Prototype.SteamEngine, reference_position = boiler.position, spacing =2, direction = Direction.UP)"
+        # no achievements
+        _, _, _, achievements = eval_program_with_achievements(instance, test_string)
+        assert len(achievements["static"].keys())==0
+        assert len(achievements["dynamic"].keys())==0
+
+        # Now we connect the steam engine and boiler but dont power boiler so still no achievements
+        test_string = "entities = get_entities()\nboiler = [entity for entity in entities if entity.name == 'boiler'][0]\nsteam_engine = [entity for entity in entities if entity.name == 'steam-engine'][0]\nconnect_entities(boiler, steam_engine, Prototype.Pipe)\nsleep(2)"
+        # no achievements
+        _, _, _, achievements = eval_program_with_achievements(instance, test_string)
+        assert len(achievements["static"].keys())==0
+        assert len(achievements["dynamic"].keys())==0
+
+        # Now we add coal and should get dynamic achievements
+        test_string = "entities = get_entities()\nboiler = [entity for entity in entities if entity.name == 'boiler'][0]\ninsert_item(Prototype.Coal,boiler, 2)\nsleep(2)"
+        # dynamic water and steam
+        _, _, _, achievements = eval_program_with_achievements(instance, test_string)
+        assert len(achievements["static"].keys())==0
+        assert len(achievements["dynamic"].keys())==2
+        assert "water" in achievements["dynamic"].keys()
+        assert "steam" in achievements["dynamic"].keys()
+
+        # Now we go and mine some coal
+        test_string = "pos = nearest(Resource.Coal)\nmove_to(pos)\nharvest_resource(pos, 10)"
+        # We check that we only get the static coal achievement
+        _, _, _, achievements = eval_program_with_achievements(instance, test_string)
+        assert len(achievements["static"].keys())==1
+        assert len(achievements["dynamic"].keys())==0
+        assert "coal" in achievements["static"].keys()
+        assert achievements["static"]["coal"]==10
+
+
+def test_achievements_electricity():
+        """
+        This test might fail in some map cases due to place_entity_next_to direction being incompatible with the nearest water source
+        This has been tested in fact lab, game speed atleast 5
+        """
+        instance = FactorioInstance(address='localhost',
+                                bounding_box=200,
+                                tcp_port=27015,
+                                fast=True,
+                                #cache_scripts=False,
+                                inventory={"offshore-pump":1,
+                                           "boiler":1,
+                                           "coal":4,
+                                           "pipe":30,
+                                           "steam-engine":1,
+                                           "small-electric-pole": 200,
+                                           "electric-mining-drill": 2})
+        # This just sets it up
+        test_string = "pos = nearest(Resource.Water)\nmove_to(pos)\npump = place_entity(Prototype.OffshorePump, position = pos)\nboiler = place_entity_next_to(Prototype.Boiler, reference_position = pump.position, spacing =2, direction = Direction.UP)\nconnect_entities(pump, boiler, Prototype.Pipe)\nsteam_engine = place_entity_next_to(Prototype.SteamEngine, reference_position = boiler.position, spacing =2, direction = Direction.UP)\nconnect_entities(boiler, steam_engine, Prototype.Pipe)\ninsert_item(Prototype.Coal,boiler, 2)\nsleep(5)"
+        # run the setup
+        _, _, _, achievements = eval_program_with_achievements(instance, test_string)
+
+
+        # Place a electric mining drill on coal patch
+        test_string = "pos = nearest(Resource.IronOre)\nmove_to(pos)\ndrill = place_entity(Prototype.ElectricMiningDrill, position = pos)"
+        # We should get no achievements
+        _, _, _, achievements = eval_program_with_achievements(instance, test_string)
+        assert len(achievements["static"].keys())==0
+        assert len(achievements["dynamic"].keys())==0
+
+        # Now connect the the drill to the steam engine
+        test_string = "entities = get_entities()\ndrill = [entity for entity in entities if entity.name == 'electric-mining-drill'][0]\nsteam_engine = [entity for entity in entities if entity.name == 'steam-engine'][0]\nconnect_entities(drill, steam_engine, Prototype.SmallElectricPole)\nsleep(15)"
+        # We should get some achievements, namely dynamic iron ore
+        _, _, _, achievements = eval_program_with_achievements(instance, test_string)
+        assert len(achievements["static"].keys())==0
+        assert len(achievements["dynamic"].keys())==1
+        assert "iron-ore" in achievements["dynamic"].keys()
+
+
 if __name__ == '__main__':
-    unittest.main()
+    #unittest.main()
+    test_achievements_electricity()
+
