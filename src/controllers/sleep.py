996a76c750fcf4c212de515dede39ba2fed4b59f
Mart <mart@paperplane.ai>
2024-12-16 16:20:40 +0000

    removed sleep

--- src/controllers/sleep.py
+++ src/controllers/sleep.py
@@ -14,17 +14,18 @@ class Sleep(Action):
         :param seconds: Number of seconds to sleep.
         :return: True if sleep was successful.
         """
-        # Get initial tick
-        start_tick, _ = self.execute()
-        target_ticks = seconds * 60  # Convert seconds to ticks (60 ticks = 1 second)
-
-        while True:
-            current_tick, _ = self.execute()
-            ticks_elapsed = current_tick - start_tick
-
-            if ticks_elapsed >= target_ticks:
-                return True
-
-            # Sleep for a small interval to prevent excessive polling
-            # Using 0.05 seconds (50ms) as a reasonable polling interval
-            sleep(0.05)
+        sleep(seconds)
+        ## Get initial tick
+        #start_tick, _ = self.execute()
+        #target_ticks = seconds * 60  # Convert seconds to ticks (60 ticks = 1 second)
+#
+        #while True:
+        #    current_tick, _ = self.execute()
+        #    ticks_elapsed = current_tick - start_tick
+#
+        #    if ticks_elapsed >= target_ticks:
+        #        return True
+#
+        #    # Sleep for a small interval to prevent excessive polling
+        #    # Using 0.05 seconds (50ms) as a reasonable polling interval
+        #    sleep(0.05)
