402231110b41c23c19ed895f38ae42c71dd19a52
Mart <mart@paperplane.ai>
2024-12-19 15:26:04 +0000

    added checks for profits and achievements

--- src/utils.py
+++ src/utils.py
@@ -135,8 +135,10 @@ def get_profits(pre_production_flows,
             return total_profits
         if "output" not in pre_production_flows or "output" not in post_production_flows:
             return total_profits
-        pre_production_flows["crafted"] = [item for item in pre_production_flows["crafted"].values()]
-        post_production_flows["crafted"] = [item for item in post_production_flows["crafted"].values()]
+        if isinstance(pre_production_flows["crafted"], dict):
+            pre_production_flows["crafted"] = [item for item in pre_production_flows["crafted"].values()]
+        if isinstance(post_production_flows["crafted"], dict):
+            post_production_flows["crafted"] = [item for item in post_production_flows["crafted"].values()]
         new_production_flows = get_new_production_flows(pre_production_flows, post_production_flows)
         # merge the crafted and harvested dicts to one dict
         static_profits, new_production_flows = get_static_profits(new_production_flows, 
@@ -259,8 +261,10 @@ def get_achievements(pre_production_flows, post_production_flows):
         return achievements
 
 def get_updated_static_items(pre_production_flows, post_production_flows):
-    pre_production_flows["crafted"] = [item for item in pre_production_flows["crafted"].values()]
-    post_production_flows["crafted"] = [item for item in post_production_flows["crafted"].values()]
+    if isinstance(pre_production_flows["crafted"], dict):
+        pre_production_flows["crafted"] = [item for item in pre_production_flows["crafted"].values()]
+    if isinstance(post_production_flows["crafted"], dict):
+        post_production_flows["crafted"] = [item for item in post_production_flows["crafted"].values()]
     new_production_flows = get_new_production_flows(pre_production_flows, post_production_flows)
     static_items = new_production_flows["harvested"]
     
