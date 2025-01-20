4e2e5fc6217165f96b40cf699628d49dc79d73ea
Mart <mart@paperplane.ai>
2025-01-20 22:40:32 +0000

    new nearest buildable

--- src/controllers/nearest_buildable.py
+++ src/controllers/nearest_buildable.py
@@ -2,7 +2,7 @@ import math
 from typing import Optional
 
 from controllers.__action import Action
-from factorio_entities import Position, BoundingBox
+from factorio_entities import Position, BuildingBox
 from factorio_instance import PLAYER
 from factorio_types import Prototype
 
@@ -16,37 +16,53 @@ class NearestBuildable(Action):
 
     def __call__(self,
                  entity: Prototype,
-                 bounding_box: Optional[BoundingBox] = None,
+                 building_box: BuildingBox,
+                 center_position: Position,
                  **kwargs
-                 ) -> Position:
+                 ) -> dict:
+        """
+        Find the nearest buildable area for an entity.
+        Inputs
+        :param entity: Prototype of the entity to build.
+        :param building_box: The bounding box of the entity to build.
+        :param center_position: The position to find the nearest area where building box fits
+        
+        Outputs
+        :return: A dictionary containing the center position of the buildable area, width and height margins
+            left_top - The top left position of the buildable area
+            right_bottom - The bottom right position of the buildable area
+            left_bottom - The bottom left position of the buildable area
+            right_top - The top right position of the buildable area
+        """
         if not isinstance(entity, Prototype):
             raise Exception("'nearest_buildable' requires the Prototype of the desired entity as the first argument")
 
-        # Convert bounding box to relative coordinates if provided
-        bb_data = None
-        dx, dy = 0, 0
-        if bounding_box:
-            dx = bounding_box.left_top.x# if bounding_box.left_top.x < 0 else 0
-            dy = bounding_box.left_top.y# if bounding_box.left_top.y < 0 else 0
-            MARGIN = 1
 
-            bb_data = {
-                "left_top": {"x": bounding_box.left_top.x-dx, "y": bounding_box.left_top.y-dy},
-                "right_bottom": {"x": bounding_box.right_bottom.x-dx+MARGIN*2, "y": bounding_box.right_bottom.y-dy+MARGIN*2},
-                "center": {"x": bounding_box.center.x-dx, "y": bounding_box.center.y-dy}
+
+
+        MARGIN = 0
+        dx = building_box.width
+        dy = building_box.height  
+        dx = dx + MARGIN
+        dy = dy + MARGIN
+        bb_data = {
+                "left_top": {"x": 0, "y": 0},
+                "right_bottom": {"x": dx, "y": dy}
             }
+        # make all the values integers
+        center_position = {"x": math.ceil(center_position.x + 0.5), "y": math.ceil(center_position.y + 0.5)}
 
-        response, time_elapsed = self.execute(PLAYER, entity.value[0], bb_data)
+
+        response, time_elapsed = self.execute(PLAYER, entity.value[0], bb_data, center_position)
 
         if isinstance(response, str):
-            raise Exception(f"No viable place to put {str(entity)} found on the map")
+            raise Exception(f"No viable place to put {str(entity)} near the centre position {center_position} with this BuildingBox size found on the map. Either decrease the size of the BuildingBox or use multiple smaller BuildingBoxes")
 
-        # if not self.game_state.last_observed_player_location:
-        #     self.game_state.last_observed_player_location = self.game_state.player_location
-        #
-        # else:
-        # return (math.floor(response['x']), math.floor(response['y']))
-        if not bounding_box:
-            return Position(x=response['x'], y=response['y'])
-        else:
-            return Position(x=response['x']-dx, y=response['y']-dy)
+        response_x = response['x']
+        response_y = response['y']
+
+        return {"left_top": Position(x=response_x, y=response_y),
+                "right_bottom": Position(x=response_x+dx-1,
+                                          y=response_y+dy-1),
+                "left_bottom": Position(x=response_x, y=response_y+dy-1),
+                "right_top": Position(x=response_x + dx-1, y=response_y)}
