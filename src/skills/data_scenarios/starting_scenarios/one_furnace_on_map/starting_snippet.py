546562408932db05d28ef49fef83e8d260351f09
Mart <mart@paperplane.ai>
2025-01-31 10:10:06 +0000

    deleted unnecessary files

--- src/skills/data_scenarios/starting_scenarios/one_furnace_on_map/starting_snippet.py
+++
@@ -1,6 +0,0 @@
-from factorio_instance import *
-
-
-furnace_pos = Position(x = -12, y = -12)
-move_to(furnace_pos)
-stone_furnace = place_entity(Prototype.StoneFurnace, Direction.UP, furnace_pos)
--- src/skills/data_scenarios/starting_scenarios/random_raw_material_plates/starting_snippet.py
+++
@@ -1,1 +0,0 @@
-from factorio_instance import *
